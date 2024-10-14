775bd2130f0a02a5d519c36f0ae5c2b3f51c458d
shameen <shameendilusha5678@gmail.com>
2024-10-14 14:43:49 +0530

    ItemService interface updated

--- src/main/java/lk/ijse/posbackendprase2/service/ItemService.java
+++ src/main/java/lk/ijse/posbackendprase2/service/ItemService.java
@@ -1,4 +1,14 @@
 package lk.ijse.posbackendprase2.service;
 
+
+import lk.ijse.posbackendprase2.dto.impl.ItemDto;
+
+import java.util.List;
+
 public interface ItemService {
+    void saveItem(ItemDto itemDto);
+    ItemDto getItemById(String itemCode);
+    List<ItemDto> getAllItems();
+    void updateItem(String itemCode, ItemDto itemDto);
+    void deleteItem(String itemCode);
 }
