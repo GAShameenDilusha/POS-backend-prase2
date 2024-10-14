a8cdba0a280eeda02c7cffed2822def598890117
shameen <shameendilusha5678@gmail.com>
2024-10-14 14:56:03 +0530

    update OrderService.java

--- src/main/java/lk/ijse/posbackendprase2/service/OrderService.java
+++ src/main/java/lk/ijse/posbackendprase2/service/OrderService.java
@@ -1,4 +1,13 @@
 package lk.ijse.posbackendprase2.service;
 
+import lk.ijse.posbackendprase2.dto.impl.OrderDto;
+
+import java.util.List;
+
 public interface OrderService {
+    void saveOrder(OrderDto orderDto);
+    OrderDto getOrderById(String orderId);
+    List<OrderDto> getAllOrders();
+    void updateOrder(String orderId, OrderDto orderDto);
+    void deleteOrder(String orderId);
 }
