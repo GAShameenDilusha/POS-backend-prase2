75ac96c24b2a853d71b6a9a3431ab0b2d008c103
shameen <shameendilusha5678@gmail.com>
2024-10-14 14:55:17 +0530

    update WebAppRootConfig

--- src/main/java/lk/ijse/posbackendprase2/config/WebAppConfig.java
+++ src/main/java/lk/ijse/posbackendprase2/config/WebAppConfig.java
@@ -1,19 +1,11 @@
 package lk.ijse.posbackendprase2.config;
 
-
-import jakarta.servlet.annotation.MultipartConfig;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
-@ComponentScan(basePackages = "lk.ijse.posbackendprase2")
 @EnableWebMvc
-@MultipartConfig(
-        fileSizeThreshold = 1024 * 1024 * 2,
-        maxFileSize = 1024 * 1024 * 5,
-        maxRequestSize = 1024 * 1024 * 10
-)
+@ComponentScan(basePackages = "lk.ijse.posbackendprase2")
 public class WebAppConfig {
-
 }
