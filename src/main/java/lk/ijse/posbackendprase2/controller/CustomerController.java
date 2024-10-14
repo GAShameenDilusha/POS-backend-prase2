70bdcc1ea650c9f7e116445b24aa6e94918e5c0a
shameen <shameendilusha5678@gmail.com>
2024-10-14 14:38:02 +0530

    CustomerController updated

--- src/main/java/lk/ijse/posbackendprase2/controller/CustomerController.java
+++ src/main/java/lk/ijse/posbackendprase2/controller/CustomerController.java
@@ -1,4 +1,83 @@
 package lk.ijse.posbackendprase2.controller;
 
+
+import lk.ijse.posbackendprase2.customStatusCodes.SelectedCustomerErrorStatus;
+import lk.ijse.posbackendprase2.dto.CustomerStatus;
+import lk.ijse.posbackendprase2.dto.impl.CustomerDto;
+import lk.ijse.posbackendprase2.exception.CustomerNotFoundException;
+import lk.ijse.posbackendprase2.exception.DataPersistException;
+import lk.ijse.posbackendprase2.service.CustomerService;
+import lk.ijse.posbackendprase2.util.RegexUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/customers")
 public class CustomerController {
+
+    @Autowired
+    private CustomerService service;
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> saveCustomer(@RequestBody CustomerDto dto) {
+        try {
+            service.saveCustomer(dto);
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        } catch (DataPersistException d) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping(value = "/{customerId}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public CustomerStatus getSelectedCustomer(@PathVariable("customerId") String customerId) {
+        if (!RegexUtil.isValidCustomerId(customerId)) {
+            return new SelectedCustomerErrorStatus(1, "Customer Not Found");
+        }
+        return service.getCustomer(customerId);
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<CustomerDto> getAllCustomers() {
+        return service.getAllCustomers();
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping(value = "/{customerId}")
+    public ResponseEntity<Void> deleteCustomer(@PathVariable("customerId") String customerId) {
+        try {
+            if (!RegexUtil.isValidCustomerId(customerId)) {
+                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            }
+            service.deleteCustomer(customerId);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (CustomerNotFoundException c) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PutMapping(value = "/{customerId}", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> updateCustomer(@PathVariable("customerId") String customerId, @RequestBody CustomerDto dto) {
+        try {
+            if (!RegexUtil.isValidCustomerId(customerId) || dto == null) {
+                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            }
+            service.updateCustomer(dto, customerId);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (CustomerNotFoundException e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
