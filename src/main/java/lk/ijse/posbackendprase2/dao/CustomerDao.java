33e74ec57aa2442e032de59290939da5bb69db15
shameen <shameendilusha5678@gmail.com>
2024-10-14 14:38:18 +0530

    update CustomerDao.java

--- src/main/java/lk/ijse/posbackendprase2/dao/CustomerDao.java
+++ src/main/java/lk/ijse/posbackendprase2/dao/CustomerDao.java
@@ -1,4 +1,9 @@
 package lk.ijse.posbackendprase2.dao;
 
-public interface CustomerDao {
+import lk.ijse.posbackendprase2.entity.impl.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerDao extends JpaRepository<Customer, String> {
 }
