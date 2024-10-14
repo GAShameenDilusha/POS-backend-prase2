dbd0046d532bfc8e8fa4019d929551936e9cd57b
shameen <shameendilusha5678@gmail.com>
2024-10-14 14:42:22 +0530

    Update ItemDao.java

--- src/main/java/lk/ijse/posbackendprase2/dao/ItemDao.java
+++ src/main/java/lk/ijse/posbackendprase2/dao/ItemDao.java
@@ -1,4 +1,9 @@
 package lk.ijse.posbackendprase2.dao;
 
-public interface ItemDao {
+import lk.ijse.posbackendprase2.entity.impl.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ItemDao extends JpaRepository<Item, String> {
 }
