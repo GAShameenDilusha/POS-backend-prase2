4e645b554e7f61a3bc4fa8844d34c9be589c90be
shameen <shameendilusha5678@gmail.com>
2024-10-14 14:39:34 +0530

    CustomerService interface updated

--- src/main/java/lk/ijse/posbackendprase2/service/CustomerService.java
+++ src/main/java/lk/ijse/posbackendprase2/service/CustomerService.java
@@ -1,4 +1,16 @@
 package lk.ijse.posbackendprase2.service;
 
+
+
+import lk.ijse.posbackendprase2.dto.CustomerStatus;
+import lk.ijse.posbackendprase2.dto.impl.CustomerDto;
+
+import java.util.List;
+
 public interface CustomerService {
+    void saveCustomer(CustomerDto dto);
+    void deleteCustomer(String id);
+    List<CustomerDto> getAllCustomers();
+    CustomerStatus getCustomer(String id);
+    void updateCustomer(CustomerDto dto, String id);
 }
