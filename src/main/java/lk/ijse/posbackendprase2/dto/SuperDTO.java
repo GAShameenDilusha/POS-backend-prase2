073ee335e8b863acb52fa8175226ce9116a8f58d
shameen <shameendilusha5678@gmail.com>
2024-10-14 14:59:56 +0530

    SuperDto class Updated

--- src/main/java/lk/ijse/posbackendprase2/dto/SuperDTO.java
+++
@@ -1,4 +0,0 @@
-package lk.ijse.posbackendprase2.dto;
-
-public interface SuperDTO {
-}
---
+++ src/main/java/lk/ijse/posbackendprase2/dto/SuperDto.java
@@ -0,0 +1,6 @@
+package lk.ijse.posbackendprase2.dto;
+
+import java.io.Serializable;
+
+public interface SuperDto extends Serializable {
+}
