52566c67a568100cf0a8451078367881928d6978
shameen <shameendilusha5678@gmail.com>
2024-10-14 17:31:03 +0530

    updated UserNotFoundException.java

---
+++ src/main/java/lk/ijse/posbackendprase2/exception/UserNotFoundException.java
@@ -0,0 +1,15 @@
+package lk.ijse.posbackendprase2.exception;
+
+public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+
+    public UserNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public UserNotFoundException(Throwable cause) {
+        super(cause);
+    }
+}
