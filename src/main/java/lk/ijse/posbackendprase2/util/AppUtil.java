f23509260ab78e85bc24db22906c10e2bfc534ea
shameen <shameendilusha5678@gmail.com>
2024-10-14 14:51:12 +0530

    update AppUtil.java

--- src/main/java/lk/ijse/posbackendprase2/util/AppUtil.java
+++ src/main/java/lk/ijse/posbackendprase2/util/AppUtil.java
@@ -1,4 +1,23 @@
 package lk.ijse.posbackendprase2.util;
 
+import java.util.UUID;
+
 public class AppUtil {
+    public static String generateUserId(){
+        return "USERID-" + UUID.randomUUID();
+    }
+
+    public static String generateCustomerId(){
+        return "CUSID-" + UUID.randomUUID();
+    }
+
+    public static String generateItemId(){
+        return "ITEMID-" + UUID.randomUUID();
+    }
+
+    public static String generateOrderId(){
+        return "ORDERID-" + UUID.randomUUID();
+    }
+
+    public static String generateOrderDetailId(){ return "ORDERDETAILID-" + UUID.randomUUID();}
 }
